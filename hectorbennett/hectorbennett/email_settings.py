7d3e1040486effe6e09dc635870881bf85fb0987
Hector Bennett <hectorbennett@outlook.com>
2017-11-14 21:18:40 +0000

    small mod

--- hectorbennett/hectorbennett/email_settings.py
+++ hectorbennett/hectorbennett/email_settings.py
@@ -2,8 +2,8 @@
 This file is where I include sensitive email passwords. Not for comitting.
 """
 
-EMAIL_HOST
-EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD
-EMAIL_PORT
-EMAIL_USE_TLS
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_HOST_USER = 'example_username'
+EMAIL_HOST_PASSWORD = 'example_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
