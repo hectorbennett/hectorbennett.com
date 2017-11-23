5145952544299b34be0df47f57ba78395bca065a
Hector Bennett <hectorbennett@outlook.com>
2017-11-23 19:48:21 +0000

    configured js linting

--- hectorbennett/main/static/scripts/secret-santa.js
+++ hectorbennett/main/static/scripts/secret-santa.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-    console.log('secret-santa.js');
-    var form_count = Number($('[name=form-TOTAL_FORMS]').val());
+    var form_count = Number($("[name=form-TOTAL_FORMS]").val());
     // get extra form count so we know what index to use for the next item.
-    
-    $('#add-another').click(function () {
+
+    $("#add-another").click(function () {
         form_count++;
-        $('#santa-forms').append($('#empty_form').html().replace(/__prefix__/g, form_count));
-        $('#id_form-TOTAL_FORMS').val(parseInt(form_count));
+        $("#santa-forms").append($("#empty_form").html().replace(/__prefix__/g, form_count));
+        $("#id_form-TOTAL_FORMS").val(parseInt(form_count));
     });
-    
-    $('body').off('click').on('click', '.delete', function() {
+
+    $("body").off("click").on("click", ".delete", function () {
         form_count--;
-        $(this).closest('.card').remove();
-        $('#id_form-TOTAL_FORMS').val(parseInt(form_count));
+        $(this).closest(".card").remove();
+        $("#id_form-TOTAL_FORMS").val(parseInt(form_count));
     });
 });
+$;
