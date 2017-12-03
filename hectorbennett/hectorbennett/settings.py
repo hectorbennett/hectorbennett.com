0cdb4f17254dea4cfef4b14f6c3f78e38f41b004
Hector Bennett <hectorbennett@outlook.com>
2017-12-03 15:35:57 +0000

    turned debug mode off

--- hectorbennett/hectorbennett/settings.py
+++ hectorbennett/hectorbennett/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secret_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '139.59.179.243', 'hectorbennett.com']
 
