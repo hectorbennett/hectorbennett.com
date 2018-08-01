86bc0c8a23fec055017c8fc90ca3fed9da26cbf9
Hector Bennett <hectorbennett@outlook.com>
2018-08-01 23:21:30 +0100

    added www to allowed hosts

--- hectorbennett/hectorbennett/settings.py
+++ hectorbennett/hectorbennett/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = secret_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '139.59.179.243', 'hectorbennett.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '139.59.179.243',
+    'hectorbennett.com',
+    'www.hectorbennett.com'
+]
 
 
 # Application definition
