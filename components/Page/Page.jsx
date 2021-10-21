d20d218274c1776093d9fec594179fef9d979a12
Hector <hectorbennett@outlook.com>
2021-10-21 21:50:59 +0100

    initial commit

---
+++ components/Page/Page.jsx
@@ -0,0 +1,41 @@
+import styles from "./Page.module.scss";
+
+export default function Page() {
+  return (
+    <div className={styles.page}>
+      <section className={styles.section}>
+        <h1 className={styles.heading}>
+          <span className={styles.highlight}>
+            Hector Bennett, Full Stack Web Developer :)
+          </span>
+        </h1>
+        <p className={styles.highlight}>
+          I'm a web developer at{" "}
+          <a href="https://www.artlogic.net" target="_blank">
+            Artlogic
+          </a>
+          . I write{" "}
+          <a href="https://github.com/hectorbennett" target="_blank">
+            nice code
+          </a>
+          . I make{" "}
+          <a
+            href="https://open.spotify.com/artist/48J4JtAcKjlqknQaZOf3Jf"
+            target="_blank"
+          >
+            music
+          </a>{" "}
+          in my spare time. I can be contacted via{" "}
+          <a href="mailto:contact@hectorbennett.com" target="_blank">
+            Email
+          </a>{" "}
+          or{" "}
+          <a href="https://uk.linkedin.com/in/hector-bennett" target="_blank">
+            LinkedIn
+          </a>
+          .
+        </p>
+      </section>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+import Page from "../components/Page";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="hectorbennett.com" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preload"
+          href="/fonts/Alice-Regular.woff"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Catamaran-Black.woff"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Droid-Sans-Mono.woff"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
+      <Page />
+    </>
+  );
+}
