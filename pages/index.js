0fb76f016e02d4bcbf940ceb51c31e19b8fbb815
Hector <hectorbennett@outlook.com>
2021-12-02 07:54:59 +0000

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,24 @@ export default function Home() {
       <Head>
         <title>Hector Bennett | Web Developer</title>
         <meta name="description" content="hectorbennett.com" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="preload"
           href="/fonts/Alice-Regular.woff"
