3ab5267eb521ab58558122e91f5185e8418de8f5
Hector <hectorbennett@outlook.com>
2022-03-16 12:57:20 +0000

    fix blurry favicon

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,15 @@ export default function Home() {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest?v=1" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=1"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
+        <meta name="msapplication-TileColor" content="#603cba" />
+        <meta name="theme-color" content="#ffffff" />
         <link
           rel="preload"
           href="/fonts/Alice-Regular.woff"
